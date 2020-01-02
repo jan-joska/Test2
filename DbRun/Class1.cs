ba47a5c9f0955948ebcab960d3028c6b77f4c825
Jan Joska <jan@joska.net>
2020-01-02 16:09:57 +0100

    initial commit

---
+++ DbRun/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DbRun
+{
+
+    // Run command in sql script file against db
+    // Run command in sql script only once against db
+    // Run one file
+    // Run folder structure recursively
+    // List files run against db
+    // Purge list 
+
+    // What is needed
+    // Connection string
+    // Database name
+    // file path
+    // folde path
+
+    // How
+    // Discover, sort by name, calculate hash, compare and run in order, remeber
+    // Update repository only when success
+
+    // Table structure
+    // Id, int , identity
+    // FileName nvarcher(1024)
+    // FileHash varbinary
+    // DateOfRun datetime
+
+    public class ConnectionInfo
+    {
+        public string ConnectionString { get; }
+
+        public ConnectionInfo(string connectionString)
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+    }
+
+    public class RunSingleFileScript
+    {
+
+    }
+
+    public class RunScriptFolder
+    {
+
+    }
+
+    public class 
+
+}
+
+
---
+++ ParserTest/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CommandLine;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace ParserTest
+{
+
+    public class ParameterParser
+    {
+
+        
+
+    }
+
+    class Program
+    {
+
+        [Verb("test", HelpText = "test")]
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('p', "path",Required = true, HelpText = "Path to a file")]
+            public string Path { get;set;}
+
+        }
+
+        [Verb("add", HelpText = "Add file contents to the index.")]
+        class AddOptions
+        { //normal options here
+        }
+        [Verb("commit", HelpText = "Record changes to the repository.")]
+        class CommitOptions
+        { //normal options here
+        }
+        [Verb("clone", HelpText = "Clone a repository into a new directory.")]
+        class CloneOptions
+        { //normal options here
+        }
+
+        public class DbRunEvidence
+        {
+            public long Id { get;set;}
+            public string FileName { get;set;}
+            public byte[] FileHash { get; set; }
+            public DateTime DateRun { get; set; }
+            public string Author { get; set; }
+            public string Location { get; set; }
+
+            public override string ToString()
+            {
+                return $"{nameof(Id)}: {Id}, {nameof(FileName)}: {FileName}, {nameof(FileHash)}: {FileHash}, {nameof(DateRun)}: {DateRun}, {nameof(Author)}: {Author}, {nameof(Location)}: {Location}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            using (IDbConnection conn = new SqlConnection(@"Server=JJ-HQ-DESKTOP\SQLEXPRESS;Database=TestDb;Trusted_Connection=True;"))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = Resource.CreateEvicenceDbScript;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            using (IDbConnection conn = new SqlConnection(@"Server=JJ-HQ-DESKTOP\SQLEXPRESS;Database=TestDb;Trusted_Connection=True;"))
+            {
+                
+                var record = new DbRunEvidence()
+                {
+                    Author = "Jan Joska",
+                    FileName = "20200101135563 - Initial script.sql",
+                    FileHash = new byte[] {255,0,255,250},
+                    Location = "system",
+                    DateRun = DateTime.Now
+
+                };
+                conn.Open();
+                conn.Insert<DbRunEvidence>(record);
+                conn.Close();
+            }
+
+            using (IDbConnection conn = new SqlConnection(@"Server=JJ-HQ-DESKTOP\SQLEXPRESS;Database=TestDb;Trusted_Connection=True;"))
+            {
+                conn.Open();
+                var records = conn.Query<DbRunEvidence>(new CommandDefinition("SELECT * FROM [dbo].[DbRunEvidences] ORDER BY [Id]")).ToArray();
+                conn.Close();
+                foreach (var record in records)
+                {
+                    Console.WriteLine(record);
+                }
+            }
+
+
+            //Parser.Default.ParseArguments<Options,AddOptions, CommitOptions, CloneOptions>(args)
+            //    .WithParsed<Options>(o =>
+            //    {
+            //        if (o.Verbose)
+            //        {
+            //            Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+            //            Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+            //            Console.WriteLine("Quick Start Example!");
+            //        }
+
+            //        Console.WriteLine(o.Path);
+
+            //    });
+            Console.ReadLine();
+        }
+    }
+}
